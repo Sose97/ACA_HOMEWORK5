4051caecd5d2ec5efa4c150a80c1e8e1ed5fd6fa
Sose97 <Sose97@users.noreply.github.com>
2017-04-21 15:11:36 +0400

    first commit

---
+++ Logger/Logger/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Logger
+{
+     static class Logger
+    {
+        static int i = 0;
+        private static Object thisLock = new Object();
+        public static void AddLog(Log current)
+        {
+            lock (thisLock)
+            {
+                current.ID = ++i;
+                
+            }
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["TestConnection"].ConnectionString))
+            {
+                string sql = "INSERT INTO Logs(ID, Time, SourceIP, Message)"+
+                             "VALUES("+current.ID+","+current.time+","+current.sourceIP+","+current.message+")";
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                SqlDataReader dr = cmd.ExecuteReader();
+            }
+
+
+
+
+
+
+        }
+    }
+}
